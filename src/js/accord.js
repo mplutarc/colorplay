0c0ad7e157753a90b477afd17096af44e0663e7c
Fatcheva Galina <gfatcheva@mbp-gfatcheva.local>
2021-07-20 13:59:26 +0300

    accord active устанавливает новый цвет сразу

--- src/js/accord.js
+++ src/js/accord.js
@@ -4,6 +4,19 @@ let i;
 for (i = 0; i < acc.length; i++) {
 	acc[i].addEventListener("click", function() {
 		this.classList.toggle("active");
+
+		let rgb = {
+			r: 0,
+			g: 0,
+			b: 0
+		}
+		if(pxData){
+			rgb = {
+				r: pxData[0],
+				g: pxData[1],
+				b: pxData[2]
+			}
+		}
 		let panel = this.nextElementSibling;
 		if (panel.style.maxHeight) {
 			panel.style.maxHeight = null;
@@ -14,5 +27,6 @@ for (i = 0; i < acc.length; i++) {
 			panel.style.padding = '0.5rem';
 			panel.style.marginBottom = '0.5rem';
 		}
+		setColor(rgb, light);
 	});
 }
